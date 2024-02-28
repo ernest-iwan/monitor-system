21ac8f77c5ce32db68812527958dbf64e0bbf01f
Ernest Iwan <iwanernest29@gmail.com>
2024-02-28 10:58:34 +0100

    celery setup

--- mysite/celery.py
+++ mysite/celery.py
@@ -1,11 +1,29 @@
 from __future__ import absolute_import, unicode_literals
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+import django
+django.setup()
 
 from celery import Celery
 from django.conf import settings
 from celery.schedules import crontab
+from monitor.models import Log
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+import requests
+import ssl
+import whois
+from datetime import datetime, timezone
+import tzlocal
+from ping3 import ping
+
+now = datetime.now()
+format_str = "%Y-%m-%d %H:%M:%S %z"
+time_zone = tzlocal.get_localzone()
+
+
+
 
 app = Celery('mysite')
 app.conf.enable_utc = False
@@ -16,8 +34,7 @@ app.autodiscover_tasks()
 
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
-    # Calls test('hello') every 10 seconds.
-    sender.add_periodic_task(10.0, test.s('hello'), name='add every 10')
+    sender.add_periodic_task(10.0, collect_data.s("https://google.com"), name='check domain google every 10 sec')
 
     # Calls test('hello') every 30 seconds.
     # It uses the same signature of previous task, an explicit name is
@@ -35,9 +52,58 @@ def setup_periodic_tasks(sender, **kwargs):
 
 @app.task
 def test(arg):
-    print(arg)
+    print("test")
 
 @app.task
 def add(x, y):
     z = x + y
     print(z)
+
+@app.task
+def collect_data(url):
+   data = {}
+   try:
+      with requests.get(url, stream=True) as response:
+         data['request_datetime'] = datetime.now(time_zone).strftime(format_str)
+         data['domain'] = url[8:]
+         data['ping'] = round(ping(data['domain'], unit='ms'))
+         data['respones_time'] = round(response.elapsed.total_seconds() * 1000)
+         w = whois.whois(data['domain'])
+
+         certificate_info_raw = response.raw.connection.sock.getpeercert(True)
+         certificate_info = response.raw.connection.sock.getpeercert()
+
+         data['cert'] = ssl.DER_cert_to_PEM_cert(certificate_info_raw)
+         data['status_code'] = response.status_code
+
+         tmp =  datetime.strptime((certificate_info["notBefore"])[0:-4], '%b %d %H:%M:%S %Y')
+         data['cert_from'] = tmp.replace(tzinfo=timezone.utc).astimezone(time_zone).strftime(format_str)
+
+         tmp =  datetime.strptime((certificate_info["notAfter"])[0:-4], '%b %d %H:%M:%S %Y')
+         data['cert_to'] = tmp.replace(tzinfo=timezone.utc).astimezone(time_zone).strftime(format_str)
+
+         if type(w.expiration_date) == list:
+            w.expiration_date = w.expiration_date[0]
+
+         data['domain_exp'] = w.expiration_date.replace(tzinfo=timezone.utc).astimezone(time_zone).strftime(format_str)
+
+         timedelta = w.expiration_date - now
+         data['days_to_domain_exp'] = timedelta.days
+
+         match data['status_code']:
+            case code if code >=100 and code <= 199:
+               data['status'] = "Informational responses"
+            case code if code >=200 and code <= 299:
+               data['status'] = "Successful responses"
+            case code if code >=300 and code <= 399:
+               data['status'] = "Redirection messages"
+            case code if code >=400 and code <= 499:
+               data['status'] = "Client error responses"
+            case code if code >=500 and code <= 599:
+               data['status'] = "Server error responses "
+
+      l = Log(domain=data['domain'], ping=data['ping'], response_time=data['respones_time'], cert=data['cert'], status_code=data['status_code'], status=data['status'], cert_from=data['cert_from'], cert_to=data['cert_to'], domain_exp=data['domain_exp'], days_to_domain_exp=data['days_to_domain_exp'])
+      l.save()
+
+   except Exception as e:
+      print(e)
