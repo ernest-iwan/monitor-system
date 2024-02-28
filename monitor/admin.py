3a164c7dd3382a26087632bcf99d40782afcc8df
Ernest Iwan <iwanernest29@gmail.com>
2024-02-28 15:58:16 +0100

    admin panel email formset add

--- monitor/admin.py
+++ monitor/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
 from .models import *
+from django.forms import BaseInlineFormSet
+
+
+class EmailValuesInline(admin.TabularInline):
+    model = EmailValues
+    extra = 1
 
 @admin.register(Monitor)
 class MonitorAdmin(admin.ModelAdmin):
     list_display = ("name", "monitor_type", "add_date", "status", "is_active")
     list_filter = ("monitor_type", "add_date", "is_active")
     search_fields = ("name",)
+    fieldsets = [('Dane', {'fields': ['name', 'monitor_type', 'request_timeout', 'interval', 'status', 'is_active']}),]
+    # TODO url or ip or domain
+    inlines = [EmailValuesInline]
+
 
 @admin.register(Log)
 class LogAdmin(admin.ModelAdmin):
@@ -16,12 +26,23 @@ class LogAdmin(admin.ModelAdmin):
 
 @admin.register(Email)
 class EmailAdmin(admin.ModelAdmin):
-    list_display = ("email", "monitor_id")
-    list_filter = ("email", "monitor_id")
+    list_display = ("email",)
+    list_filter = ("email",)
     search_fields = ("email",)
+    
+# class PersonAdmin(admin.ModelAdmin):
+#     list_display = ('lastName', 'firstName','email')
+#     fieldsets = [
+#        ('Name',        {'fields': ['firstName', 'lastName']}),
+#        ('Contact Info',{'fields': ['email','phoneNumber']})
+#     ]
+#     inlines = [EmailInline]
+# admin.site.register(Person, PersonAdmin)
+# admin.site.register(Attribute)
 
 # @admin.register(StatusPage)
 # class StatusPageAdmin(admin.ModelAdmin):
 #     list_display = ("request_date", "domain", "ping", "response_time", "status")
 #     list_filter = ("request_date", "domain")
 #     search_fields = ("domain", "status")
+
--- monitor/models.py
+++ monitor/models.py
@@ -3,6 +3,18 @@ from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext as _
 
+
+
+class Email(models.Model):
+    email = models.CharField(_("Email"), max_length=100)
+
+    class Meta:
+        ordering = ["email"]
+        verbose_name = "Email"
+    
+    def __str__(self):
+        return _(f"{self.email}")
+
 class Monitor(models.Model):
     TYPE_HTTP = "http request"
     TYPE_PING = "ping"
@@ -24,11 +36,15 @@ class Monitor(models.Model):
 
     name = models.CharField("Nazwa", max_length=100)
     monitor_type = models.CharField(_("Typ monitorowania"), max_length=15, choices=TYPES, default=TYPE_HTTP)
-    request_timeout = models.FloatField(_("Czas odpowiedzi"))
+    request_timeout = models.FloatField(_("Czas oczekiwania na rządanie"))
     interval = models.FloatField(_("Interwał"))
     add_date = models.DateTimeField(_("Data dodania"), auto_now_add=True)
     status = models.CharField(_("Status"), max_length=15, choices=STATUSES, default=STATUS_ONLINE)
     is_active = models.BooleanField(_("Czy aktywny?"))
+    emails = models.ManyToManyField(Email, verbose_name=_("Emaile"))
+    url = models.CharField(_("URL"), max_length=100, null=True)
+    domain_or_ip = models.CharField(_("Domena lub IP"), max_length=100, null=True)
+
 
     class Meta:
         ordering = ['add_date']
@@ -38,16 +54,16 @@ class Monitor(models.Model):
     def __str__(self):
         return _(f"{self.name} - {self.monitor_type} - ({self.status})")
 
-class Email(models.Model):
-    email = models.CharField("Email", max_length=100)
-    monitor_id = models.ForeignKey(Monitor, on_delete=models.CASCADE, verbose_name=_("ID Monitora"))
+class EmailValues(models.Model):
+    monitor_id = models.ForeignKey(Monitor, verbose_name=_("ID Monitora"), on_delete=models.CASCADE)
+    email  = models.ForeignKey(Email, verbose_name=_("Email"), on_delete=models.PROTECT)
 
     class Meta:
-        ordering = ["monitor_id"]
-        verbose_name = "Email"
+        ordering = ["email"]
+        verbose_name = "Emaile"
     
     def __str__(self):
-        return _(f"{self.monitor_id} - {self.email}")
+        return _(f"{self.email} - {self.monitor_id}")
 
 class Log(models.Model):
     request_date = models.DateTimeField("Data zapytania" ,auto_now_add=True)
